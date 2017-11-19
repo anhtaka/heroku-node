1aa31db021a0e582e8f952694e656187c8991f70
anhtaka <email@example.com>
2017-11-19 11:58:37 +0900

    test

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8080);
- 
-console.log('Server running at http://localhost:3000/');
+var http = require('http'); //httpモジュール呼び出し
+http.createServer(function (request, response) {
+    // リクエストを受けると以下のレスポンスを送信する
+    response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
+    response.end('Hello World\n'); // レスポンスボディに書き込み＆レスポンス送信を完了する
+}).listen(process.env.PORT || 8080); //公開ポートで待ち受け
